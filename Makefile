6ffb718146469fd6bde966ad5fa3867fcdbc49ba
benetraco <benet.ramio@estudiantat.upc.edu>
2022-11-17 10:08:40 +0100

    Upddate

--- Makefile
+++ Makefile
@@ -2,4 +2,17 @@ all: comp exc
 comp: 
 	g++ -O2 -std=c++17 exh.cc
 exc: 
-	./a.out in.txt out.txt
+	./a.out ./easy/easy-1.txt ./sol_easy/sol_easy-1.txt
+	./a.out ./easy/easy-2.txt ./sol_easy/sol_easy-2.txt
+	./a.out ./easy/easy-3.txt ./sol_easy/sol_easy-3.txt
+	./a.out ./easy/easy-4.txt ./sol_easy/sol_easy-4.txt
+	./a.out ./easy/easy-5.txt ./sol_easy/sol_easy-5.txt
+	./a.out ./easy/easy-6.txt ./sol_easy/sol_easy-6.txt
+	./a.out ./easy/easy-7.txt ./sol_easy/sol_easy-7.txt
+	./a.out ./easy/easy-8.txt ./sol_easy/sol_easy-8.txt
+	./a.out ./easy/easy-9.txt ./sol_easy/sol_easy-9.txt
+	./a.out ./easy/easy-10.txt ./sol_easy/sol_easy-10.txt
+
+cal:
+	g++ -O2 -std=c++17 calculate_penalizations.cc
+	./a.out ./cal_pen/easy-1.txt
--- greedy.cc
+++ greedy.cc
@@ -9,6 +9,8 @@ using namespace std::chrono;
 using namespace std;
 
 using VI = vector<int>;
+using VD = vector<double>;
+using MD = vector<VD>;
 
 double now()
 {
@@ -36,7 +38,14 @@ struct Pen
   queue<int> q; // cua que conté els últims ne elements
 };
 
-void write_solution(const VI &current_sol, int pen, const double &time, const string &s)
+struct lakjdfaklj
+{
+  int id, left;
+  int costs;
+}
+
+void
+write_solution(const VI &current_sol, int pen, const double &time, const string &s)
 {
   ofstream f = open(s);
   f << pen << " " << time << endl;
@@ -53,8 +62,84 @@ void write_solution(const VI &current_sol, int pen, const double &time, const st
   f.close();
 }
 
-void greedy()
+int calculate_cost(const VI &ce, const VI &ne, const VI &imp_i, const VI &imp_j)
 {
+  int cost = 0;
+  int M = imp_i.size();
+  for (int k = 0; k < M; ++k)
+    if (imp_i[k] == imp_j[k])
+      if (ne[k] == 2)
+        cost += 1;
+      else
+        cost += ce[k] / 2 * ne[k];
+  return cost;
+}
+
+VI gen_sol(int C, int &pen, const VI &ce, const VI &ne, const vector<Class> &classes, const string &s)
+{
+  // Generate data structues
+  int K = classes.size();
+  int M = ne.size();
+  VI solution(C);
+  VI cars_left(K);
+  for (int i = 0; i < K; ++i)
+    cars_left[i] = classes[i].n;
+  vector<Pen> pens(M);
+  for (int i = 0; i < M; ++i)
+  {
+    pens[i].sum = 0;
+    for (int j = 0; j < ne[i]; ++j)
+      pens[i].q.push(0);
+  }
+  MD costs(C, VD(C));
+  for (int i = 0; i < C; ++i)
+    for (int j = 0; j < C; ++j)
+      costs[i][j] = costs[j][i] = calculate_cost(ce, ne, classes[i].imp, classes[j].imp);
+
+  // Calculate solution
+
+  /* Algorisme:
+  1r:
+  int k = 0
+  id = car = 0
+  for car_it in cars_left:
+    if car_it > car:
+      car = car_it
+      id = i
+  solution[k] = id;
+  ++k;
+
+  2n:
+  //calculate ce_max
+  while(k <= ce_max){
+    solution[k] = priorize_id_more_cars_left
+    if equal then priorize_id_less_cost
+    if equal then smaller_id
+    ++k;
+    actualize_pen;
+  }
+
+  3r:
+  while(k < C){
+    solution[k] = priorize_id_less_cost
+    if equal then priorize_id_more_cars_left
+    if equal then smaller_id
+    ++k;
+    actualize_pen;
+  }
+
+  */
+  return solution;
+}
+
+void greedy(const string &s, int C, const VI &ce, const VI &ne, const vector<Class> &classes)
+{
+  int pen = 0;
+  double start = now();
+  VI solution = gen_sol(C, pen, ce, ne, classes, start, s);
+  double end = now();
+  double elapsed_time = end - start;
+  write_solution(solution, pen, elapsed_time, s);
 }
 
 void read_input(ifstream &f, int &C, int &M, int &K, VI &ce, VI &ne, vector<Class> &classes)
@@ -90,5 +175,5 @@ int main(int argc, char *argv[])
 
   read_input(f, C, M, K, ce, ne, classes);
 
-  greedy();
+  greedy(argv[2], C, ce, ne, classes);
 }
