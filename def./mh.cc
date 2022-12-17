b96ae3eb4127ad25b7263589494b0cfe9a6af0f0
Carlos Arbonés Sotomayor <102261662+carlosarboness@users.noreply.github.com>
2022-12-17 14:11:48 +0100

    Add files via upload

--- def./mh.cc
+++ def./mh.cc
@@ -5,37 +5,24 @@
 #include <cassert>
 #include <fstream>
 #include <algorithm>
+#include <random>
 using namespace std;
 
-struct Class;
-struct Station;
-
 using VI = vector<int>;
-using VD = vector<double>;
-using VC = vector<Class>;
-using VS = vector<Station>;
-using MD = vector<VD>;
-using is = ifstream;
-using os = ofstream;
+using MI = vector<vector<int>>;
 
-int UNDEF = -1;
-
-struct Class
+struct Window
 {
-  int id, ncars;
-  VI improvements;
+  VI ce, ne; // Stores the ce (ne) of a particular station in the position of the vector
 };
 
-struct Station
+// Stores all the information given by the input
+struct Data
 {
-  int requirements;
-  VI line;
-};
-
-struct fit
-{
-  int id_car = UNDEF, cl = UNDEF;
-  double cost = UNDEF;
+  int C, M, K;     // Number of cars, improvements and classes respectively
+  Window w;        // Stores the windows informations
+  MI improvements; // Matrix of integers (1/0) that indicate if the i-th class requires j-th improvment
+  VI cleft;
 };
 
 double now()
@@ -43,10 +30,10 @@ double now()
   return clock() / double(CLOCKS_PER_SEC);
 }
 
-os open(const string &out)
+ofstream open(const string &out)
 {
-  os f;
-  f.open(out, os::out | os::trunc);
+  ofstream f;
+  f.open(out, ofstream::out | ofstream::trunc);
   f.setf(ios::fixed);
   f.precision(1);
   return f;
@@ -54,7 +41,7 @@ os open(const string &out)
 
 void write_solution(const VI &cs, int cp, const double &elapsed_time, const string &out)
 {
-  os f = open(out);
+  ofstream f = open(out);
   f << cp << " " << elapsed_time << endl;
 
   f << cs[0];
@@ -66,232 +53,202 @@ void write_solution(const VI &cs, int cp, const double &elapsed_time, const stri
   f.close();
 }
 
-/* Updates each individual line and retuns the penalitzations of it */
-int add_car(int bit, Station &st, int ce, int ne, bool end)
+/* Returns the penalization produced in the station -station- */
+int count_penalization(const VI &station, int ce, int ne)
 {
 
-  // update the window
-  int uw = st.line.size() - ne;
-  if (uw >= 0)
-    st.requirements -= st.line[uw];
+  int total_penalizations, requirements;
+  total_penalizations = requirements = 0;
 
-  st.line.push_back(bit);
-  st.requirements += bit;
-
-  int penalitzation = max(st.requirements - ce, 0);
-  // we add to the penalitzarions the final windows
-  if (end)
+  // count inicial windows
+  int uw = 0;
+  for (int i = ne; i > 0; --i)
   {
+    requirements += station[uw];
+    total_penalizations += max(requirements - ce, 0);
     ++uw;
-    for (; st.requirements > 0; ++uw)
-    {
-      st.requirements -= st.line[uw];
-      penalitzation += max(st.requirements - ce, 0);
-    }
   }
-  return penalitzation;
+
+  // count mid penalizations
+  for (; uw < station.size(); ++uw)
+  {
+    requirements += station[uw];
+    requirements -= station[uw - ne];
+    total_penalizations += max(requirements - ce, 0);
+  }
+
+  // count end windows
+  while (requirements > 0)
+  {
+    requirements -= station[uw - ne];
+    total_penalizations += max(requirements - ce, 0);
+    ++uw;
+  }
+
+  return total_penalizations;
 }
 
-int update_station(const VI &imp, VS &stations, const VI &ce, const VI &ne, bool end)
+/* Generates a stations representation matrix. Each row is a different station, with values
+{0, 1} in each position, depending if the there is an improvement in that position or not.
+Each column is a different class vector of improvements, depending on the -solution- vector */
+MI generate_stations(const VI &solution, const MI &improvements)
 {
-  int M = ce.size();
-  int total_penalization = 0;
-  for (int i = 0; i < M; ++i)
-    total_penalization += add_car(imp[i], stations[i], ce[i], ne[i], end);
-  return total_penalization;
-}
 
-/* Returns a vector with the inicialized stations of the algorithm. Firstly the requiremnts are
-equal to zero and the line is empty */
-VS inicialize_stations(int C, int M)
-{
-  VS stations(M);
-
-  int requirements = 0;
-  VI inicial_line;
-  inicial_line.reserve(C);
-
-  for (Station &st : stations)
-    st = Station{requirements, inicial_line};
-
+  int C = solution.size();
+  int M = improvements[0].size();
+  MI stations(M, VI(C));
+  for (int i = 0; i < C; ++i)
+    for (int j = 0; j < M; ++j)
+      stations[j][i] = improvements[solution[i]][j];
   return stations;
 }
 
-int count_penalization(const VI &sol, int C, const VI &ce, const VI &ne, const VC &classes)
+/* Counts the penalization of the solution in P */
+int penalization(const VI &solution, const Data &data)
 {
-  int K = classes.size();
-  int M = ne.size();
-  VS stations = inicialize_stations(C, M);
+  MI stations = generate_stations(solution, data.improvements);
 
+  // for each station count its penalziation
   int total_penalization = 0;
-  for (int i = 0; i < C; ++i)
-    total_penalization += update_station(classes[sol[i]].improvements, stations, ce, ne, i == C - 1);
+  for (int i = 0; i < data.M; ++i)
+    total_penalization += count_penalization(stations[i], data.w.ce[i], data.w.ne[i]);
+
   return total_penalization;
 }
 
-// returns the argmax of VI
-int most_cleft_index(const VI &cleft)
+/* Selects a random neighbour from the solution -current_solution-. The defined neighbourhood
+is all the solutions that can be reachead by permutating 2 elements of -current_solution- */
+VI select_neighbour(const VI &current_solution)
 {
-  auto it = max_element(cleft.begin(), cleft.end());
-  return distance(cleft.begin(), it);
+  int C = current_solution.size();
+  VI neighbour = current_solution;
+  swap(neighbour[rand() % C], neighbour[rand() % C]);
+  return neighbour;
 }
 
-int best_fit(VI &cleft, const VD &costs)
-{
-  fit bf;
+/* Simulated Annealing algorithm consists of following steps:
 
-  for (int i = 0; i < cleft.size(); ++i)
-    if (costs[i] < bf.cost or cleft[i] > bf.cl)
-      bf = fit{i, cleft[i], costs[i]};
+Step 1: Initialize – Start with a random initial solution and set initial parameters t0, a.
 
-  --cleft[bf.id_car];
-  return bf.id_car;
-}
+Step 2: Move – Select a random neighbour from the current solution.
 
-double calculate_cost(const VI &ce, const VI &ne, const VI &imp_i, const VI &imp_j)
-{
-  double cost = 0;
+Step 3: Calculate score – calculate the change in performance due to the move made.
 
-  for (int k = 0; k < ce.size(); ++k)
-    if (imp_i[k] == 1 and imp_j[k] == 1)
-      cost += ne[k] / ce[k];
+Step 4: Choose – Depending on the change in performance, accept or reject the move. The prob of acceptance depending on the current “temperature”.
 
-  return cost;
-}
+Step 5: Update and repeat – Update the temperature value by lowering it by a factor a.
 
-MD generate_costs_matrix(int K, const VI &ce, const VI &ne, const VC &classes)
-{
-  MD costs(K, VD(K));
-  for (int i = 0; i < K; ++i)
-    for (int j = i; j < K; ++j)
-    {
-      double c = calculate_cost(ce, ne, classes[i].improvements, classes[j].improvements);
-      costs[i][j] = costs[j][i] = c;
-    }
-  return costs;
-}
+Go back to Step 2.
 
-/* Returns a vector containing for each index the number of cars that are left for that class */
-VI count_cleft(const VC &classes)
-{
-  int K = classes.size();
-  VI cleft(K);
-  for (int i = 0; i < K; ++i)
-    cleft[i] = classes[i].ncars;
-  return cleft;
-}
+The process is done until “Freezing Point” is reached.
 
-VI gen_sol(int C, const VI &ce, const VI &ne, const VC &classes)
+Acceptance criteria:
+
+ - If the performance of neighboring state is better than current state (delta >= 0) - Accept
+ - If the performance of neighboring state is worse than current state (delta < 0) - Accept with probability e^{-delta/t} where, t is current temperature and delta is the performance difference between the current state and the neighboring state
+
+ */
+
+void run_algorithm(VI current_solution, int current_pen, double t, const double &a, const Data &data, const double &start, const string &output_file)
 {
 
-  VI cleft = count_cleft(classes);
-  MD costs = generate_costs_matrix(classes.size(), ce, ne, classes);
+  VI best_solution = current_solution; // saves the better solution
+  int best_penalization = current_pen; // saves the better solution penalization
 
-  // Calculate solution
-  VI solution(C);
-  solution[0] = most_cleft_index(cleft);
-  --cleft[solution[0]];
-
-  for (int k = 1; k < C; ++k)
-    solution[k] = best_fit(cleft, costs[solution[k - 1]]);
-
-  return solution;
-}
-
-bool improve(VI &solution, int &penalization, const VI &ce, const VI &ne, const VC &classes)
-{
-  int C = solution.size();
-
-  bool first_improvement = false;
-
-  if (first_improvement)
+  while (now() - start < 60)
   {
-    for (int i = 0; i < C; ++i)
+    //  select a neighbour from the current solution
+    VI neighbour = select_neighbour(current_solution);
+    int pen = penalization(neighbour, data); // calculate the penalization of the neighbour
+    int delta = current_pen - pen;           // difference of performance
+
+    if (delta >= 0) // the neighbour solution is better than the current one
     {
-      for (int j = 0; j < C; ++j)
+      current_solution = neighbour;
+      current_pen = pen;
+
+      if (current_pen < best_penalization)
       {
-        if (i != j)
-        {
-          VI solutionMH = solution;
-          solutionMH[j] = solution[i];
-          solutionMH[i] = solution[j];
-          if (int cp = count_penalization(solutionMH, C, ce, ne, classes); cp < penalization)
-          {
-            solution = solutionMH;
-            penalization = cp;
-            return true;
-          }
-        }
+        best_solution = current_solution;
+        best_penalization = current_pen;
+        write_solution(best_solution, best_penalization, now() - start, output_file);
       }
     }
-    return false;
-  }
-  else
-  {
-    int bpenalization;
-    VI bsolutionMH;
-
-    for (int i = 0; i < C; ++i)
+    else // the neighbour solution is worse than the current one
     {
-      for (int j = 0; j < C; ++j)
+      double rndNumber = rand() / (double)RAND_MAX; // random number in range [0, 1)
+      if (rndNumber < exp(delta / t))               // acceptance of a worsening move probability
       {
-        if (i != j)
-        {
-          VI solutionMH = solution;
-          solutionMH[j] = solution[i];
-          solutionMH[i] = solution[j];
-          if (int cp = count_penalization(solutionMH, C, ce, ne, classes); cp < penalization)
-          {
-            bpenalization = cp;
-            bsolutionMH = solutionMH;
-          }
-        }
+        current_solution = neighbour;
+        current_pen = pen;
       }
     }
-    if (bpenalization < penalization)
-    {
-      solution = bsolutionMH;
-      penalization = bpenalization;
-      return true;
-    }
-    else
-      return false;
+
+    t = t * a; // update (decrease) the temperature
   }
 }
 
-void greedy(const string &s, int C, const VI &ce, const VI &ne, const VC &classes)
+/* Generates an initial random solution for the problem. First, all the classes defined by the problem are put in order, with their respective multiplicity. For example : s = {0, 0, 1, 1, 1, 2, 2, 2, 2, 2}. Then, in order to have a better distributed initial solution a random permutation is done to vector.
+For example : s = {2, 0, 1, 1, 0 ,2, 2, 2, 1, 2} */
+VI initial_solution(const Data &data)
 {
-  double start = now();
-  VI solution = gen_sol(C, ce, ne, classes);
-  int penalization = count_penalization(solution, C, ce, ne, classes);
+  VI s;
+  s.reserve(data.C);
 
-  while (improve(solution, penalization, ce, ne, classes))
-    ;
-  double end = now();
+  for (int class_id = 0; class_id < data.K; ++class_id) // for each class
+    for (int m = 0; m < data.cleft[class_id]; ++m)      // for each multiplicity
+      s.push_back(class_id);                            // push_back of the class
 
-  write_solution(solution, penalization, end - start, s);
+  random_shuffle(s.begin(), s.end()); // random permutation of the elements in s
+  return s;
 }
 
-void read_input(is &in, int &C, int &M, int &K, VI &ce, VI &ne, VC &classes)
+/* Annealing parameters:
+
+1. s - initial solution: An initial solution to the problem is created in order to start the algorithm.
+
+2. p - cost, penalization, performance ... : Evaluation of the solution to compare it with others.
+
+3. t0 - initial temperature: Due to the way the probability is calculated, when the temperature is higher, is it more likely that the algorithm accepts a worse solution. This promotes Exploration of the search space and allows the algorithm to more likely travel down a sub-optimal path to potentially find a global maximum.
+
+4. a - geometric parameter: Factor by which temperature is scaled. Lower values of a restrict the search space at a faster rate than higher values.
+
+*/
+void simulated_annealing(const Data &data, const string &output_file)
 {
+  VI s = initial_solution(data); // create random initial solution
+  int p = penalization(s, data); // calculate its penalization
+
+  double t0 = 100; // set inicial temperature
+  double a = 0.95; // temperature reduction will follow a geometric rule: t = t * a
+
+  double start = now(); // inicialize counter
+
+  run_algorithm(s, p, t0, a, data, start, output_file);
+}
+
+void read_input(ifstream &in, Data &data)
+{
+  auto &[C, M, K, w, improvements, cleft] = data;
+  auto &[ce, ne] = w;
   in >> C >> M >> K;
 
   ce.resize(M);
   ne.resize(M);
-  classes.resize(K);
+  cleft.resize(K);
+  improvements.resize(K, VI(M));
 
   for (auto &capacity : ce)
     in >> capacity;
   for (auto &window : ne)
     in >> window;
 
+  int aux;
   for (int i = 0; i < K; ++i)
   {
-    in >> classes[i].id >> classes[i].ncars;
-    VI improvements(M);
-    for (auto &bit : improvements)
-      in >> bit;
-    classes[i].improvements = improvements;
+    in >> aux >> cleft[i];
+    for (int j = 0; j < M; ++j)
+      in >> improvements[i][j];
   }
 }
 
@@ -304,13 +261,10 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
-  ifstream in(argv[1]);
+  ifstream input_file(argv[1]);
 
-  int C, M, K;
-  VI ce, ne;
-  VC classes;
+  Data data;
+  read_input(input_file, data);
 
-  read_input(in, C, M, K, ce, ne, classes);
-
-  greedy(argv[2], C, ce, ne, classes);
+  simulated_annealing(data, argv[2]);
 }
