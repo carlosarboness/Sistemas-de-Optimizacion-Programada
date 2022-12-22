a3bf5d6856210171f1e21f8bddc4d51eff2746d0
Benet Ramió i Comas <101699331+benetraco@users.noreply.github.com>
2022-12-22 12:09:51 +0100

    Update greedy.cc

--- def./greedy.cc
+++ def./greedy.cc
@@ -15,6 +15,15 @@ struct Station;
 using VI = vector<int>;
 using MI = vector<VI>;
 using VS = vector<Station>;
+using VD = vector<double>;
+using MD = vector<VD>;
+
+/* ------- GLOBAL VARIABLES ------- */
+
+double start;
+string output_file;
+
+/* -------------------------------- */
 
 struct Window
 {
@@ -27,6 +36,7 @@ struct Data
     int C, M, K;     // Number of cars, improvements and classes respectively
     Window w;        // Stores the windows informations
     MI improvements; // Matrix of integers (1/0) that indicate if the i-th class requires j-th improvment
+    VI cleft;        // Vector that saves the cars left of the class i
 };
 
 // Stores the number of needed requirements in a particular
@@ -36,34 +46,34 @@ struct Station
     VI line;          // stores the requirements in order that need to be satisfied by the station in a particular moment
 };
 
-// C
+/* Returns the value in seconds of the processor time consumed by the program. */
 double now()
 {
     return clock() / double(CLOCKS_PER_SEC);
 }
-// C
-ofstream open(const string &out)
+
+/* Opens the file -output_file- removing all the data that has in it */
+ofstream open_truncated_file(const string &output_file)
 {
-    ofstream f;
-    f.open(out, ofstream::out | ofstream::trunc);
-    f.setf(ios::fixed);
-    f.precision(1);
-    return f;
+    ofstream out;
+    out.open(output_file, ofstream::out | ofstream::trunc);
+    out.setf(ios::fixed);
+    out.precision(1);
+    return out;
 }
 
-// C /*  */
-void write_solution(const VI &cs, int cp, const double &elapsed_time, const string &out)
+/* Writes the solution with its penalization and the time used to find that solution (elapsed_time)*/
+void write_solution(const VI &solution, int penalization, const double &elapsed_time)
 {
-    ofstream f = open(out);
-    f << cp << " " << elapsed_time << endl;
+    ofstream out = open_truncated_file(output_file);
+    out << penalization << " " << elapsed_time << endl;
 
-    f << cs[0];
-    for (int i = 1; i < cs.size(); ++i)
-        f << " " << cs[i];
+    out << solution[0];
+    for (int i = 1; i < solution.size(); ++i)
+        out << " " << solution[i];
+    out << endl;
 
-    f << endl;
-
-    f.close();
+    out.close();
 }
 
 /* Updates each individual line with the bit indicating if the improvement
@@ -138,75 +148,80 @@ given the current state of the stations */
 int try_station(const VI &imp, VS stations, const Window &w, bool end)
 {
     const auto &[ce, ne] = w;
-
     int M = ce.size();
+
     int total_penalization = 0;
+
     for (int i = 0; i < M; ++i)
         total_penalization += update_station(imp[i], stations[i], ce[i], ne[i], end);
+
     return total_penalization;
 }
 
-/* Returns the best class of cars to be improved next following the greedy algoritm.
-It also updates the stations and cleft */
-int less_pen(VI &cleft, const Data &data, const VS &stations, bool end)
+/* Returns the best class of cars to be improved next following the greedy algoritm */
+int less_pen(const Window &w, const MI &improvements, VI &cleft, const VS &stations, bool end)
 {
-    const auto &[C, M, K, w, improvements] = data;
+    int K = cleft.size();
 
-    int id = 0;
-    int pen = INT_MAX;
-    for (int i = 0; i < K; ++i)
+    int id = 0;        // stores the id of the class of cars that will be improved next
+    int pen = INT_MAX; // stores the penalization improving a car of class id next, INT_MAX works as infinity
+
+    for (int i = 0; i < K; ++i) // for each class of cars
     {
-        if (cleft[i] > 0)
+        if (cleft[i] > 0) // check if there is cars left of that class
         {
-            int c = try_station(improvements[i], stations, w, end);
-            if (c < pen or (c == pen and cleft[i] > cleft[id]))
+            // penalization of adding that class of car
+            int p = try_station(improvements[i], stations, w, end);
+
+            // check if the penalization is smaller than an id found before or if it's equal there is more cars left
+            if (p < pen or (p == pen and cleft[i] > cleft[id]))
             {
-                pen = c;
+                pen = p;
                 id = i;
             }
         }
     }
-    --cleft[id];
     return id;
 }
 
 /* Given the data of the problem, the solution using the greedy algorithm 1 is returned
 and the penalization of the solution is stored in pen */
-VI gen_sol_greedy(const Data &data, VI &cleft, int &pen)
+VI gen_sol_greedy(const Data &data, int &pen)
 {
-    const auto &[C, M, K, w, improvements] = data; //
+    auto [C, M, K, w, improvements, cleft] = data; // get the data
 
-    VI solution(C);
-    VS stations = inicialize_stations(C, M);
+    VI solution(C);                          // current solution of the problem, empty at the beginning
+    VS stations = inicialize_stations(C, M); // vector of empty stations that will be filled during the search
 
-    // The first car to be improved is the one of the class with mores cars left
+    // the first car to be improved is the one of the class with mores cars left
     solution[0] = most_cleft_index(cleft);
-    --cleft[solution[0]];
-    pen += UPL(improvements[solution[0]], stations, w, false);
+    --cleft[solution[0]];                                      // update the cleft of that class
+    pen += UPL(improvements[solution[0]], stations, w, false); // update the stations of of improving that car
 
+    // fill the rest of the solution with the car following the greedy algorithm and keep updating the penalization
     for (int k = 1; k < C; ++k)
     {
-        solution[k] = less_pen(cleft, data, stations, k == C - 1);
+        solution[k] = less_pen(w, improvements, cleft, stations, k == C - 1);
+        --cleft[solution[k]];
         pen += UPL(improvements[solution[k]], stations, w, k == C - 1);
     }
 
     return solution;
 }
 
-/* Given a data and the output file name writes in the output file the best
-solution of running two greedy algorithms */
-void greedy(const Data &data, VI &cleft, const string &out)
+/* Given a data and the output file name writes in the output file the solution of the greedy algorithm */
+void greedy(const Data &data)
 {
-    double start = now();
-    int pen = 0;
-    VI sol = gen_sol_greedy(data, cleft, pen);
-    write_solution(sol, pen, now() - start, out);
+    double start = now(); // inicialize the counter
+    int pen = 0;          // inicial penalization
+    VI sol = gen_sol_greedy(data, pen);
+    write_solution(sol, pen, now() - start);
 }
 
-// C
-void read_input(ifstream &in, Data &data, VI &cleft)
+/* Reads the input from the input_file and saves it in data */
+void read_input(ifstream &in, Data &data)
 {
-    auto &[C, M, K, w, improvements] = data;
+    auto &[C, M, K, w, improvements, cleft] = data;
     auto &[ce, ne] = w;
     in >> C >> M >> K;
 
@@ -231,18 +246,19 @@ void read_input(ifstream &in, Data &data, VI &cleft)
 
 int main(int argc, char *argv[])
 {
-    // An error sentence will be printed if there is not the required information in the command line
+    // an error sentence is printed if there is not the required information in the command line
     if (argc != 3)
     {
         cout << "Syntax: " << argv[0] << " input_file output_file" << endl;
         exit(1);
     }
 
-    ifstream in(argv[1]);
+    ifstream input_file(argv[1]);
+    output_file = argv[2];
+
     Data data;
-    VI cleft; // Stores the number of cars left of the i-th class that still need to be produced
 
-    read_input(in, data, cleft); // The imput infromation of the imput file in will be readed and stored int data and cleft
+    read_input(input_file, data); // the imput infromation of the imput file in will be readed and stored int data and cleft
 
-    greedy(data, cleft, argv[2]); // Solution given by the greedy algorithm is written in the output file argv[2]
+    greedy(data);
 }
